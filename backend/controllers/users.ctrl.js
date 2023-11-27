0452f83f2e58e34342fc9021cf5998fdb23820b2
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2023-11-28 00:06:01 +0100

    format files

--- backend/controllers/users.ctrl.js
+++ backend/controllers/users.ctrl.js
@@ -24,12 +24,10 @@ exports.createUser = async (req, res) => {
     // Create new user
     const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
-    res
-      .status(201)
-      .json({
-        message: "User created successfully.",
-        user: { username, email },
-      });
+    res.status(201).json({
+      message: "User created successfully.",
+      user: { username, email },
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/users.ctrl.js");
 
-router.post('/', userController.createUser);
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.post("/", userController.createUser);
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getUserById);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
