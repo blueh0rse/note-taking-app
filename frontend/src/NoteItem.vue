236335e609ac46443233395964fb16de1f519a83
Jpmorcos <jpmorcosdouaihy2002@hotmail.com>
2023-11-28 22:01:41 -0500

    Display, style and linking features

---
+++ frontend/src/NoteItem.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="note-item" @click="emitSelectNote">
+      <h3>{{ note.title }}</h3>
+      <p>{{ note.content }}</p>
+      <button @click="confirmDeletion(note.id)" class="delete-note-button">Delete</button>
+    </div>
+</template>
+  
+<script>
+  export default {
+    props: {
+      note: {
+        type: Object,
+        required: true
+      }
+    },
+    methods: {
+        confirmDeletion(noteId) {
+            if (confirm('Are you sure you want to delete this note?')) {
+            this.$emit('delete-note', noteId);
+            }
+        },
+        emitSelectNote() {
+        this.$emit('select-note', this.note);
+        },
+    }
+  };
+</script>
+  
+<style>
+.note-item {
+  cursor: pointer;
+  /* Additional styles... */
+}
+</style>
