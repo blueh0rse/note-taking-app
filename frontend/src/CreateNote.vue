8811d240bda91cc7088d73e3affe1033d37a7cc9
Jpmorcos <jpmorcosdouaihy2002@hotmail.com>
2023-11-28 19:47:27 -0500

    Create note first commit

---
+++ frontend/src/CreateNote.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="create-note">
+      <h2>Create New Note</h2>
+      <form @submit.prevent="createNote">
+        <div>
+          <label for="title">Title:</label>
+          <input type="text" id="title" v-model="title" required>
+        </div>
+        <div>
+          <label for="content">Content:</label>
+          <textarea id="content" v-model="content" required></textarea>
+        </div>
+        <button type="submit">Create Note</button>
+      </form>
+    </div>
+</template>
+  
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      title: '',
+      content: ''
+    };
+  },
+  methods: {
+    createNote() {
+      if (!this.title || !this.content) {
+        alert('Please fill in all fields.');
+        return;
+      }
+
+      axios.post('http://localhost:3000/notes', {
+        title: this.title,
+        content: this.content
+      })
+      .then(() => {
+        alert('Note successfully created.');
+        // Clear form or redirect user
+        this.title = '';
+        this.content = '';
+        // this.$router.push('/path-to-view-note');
+      })
+      .catch(error => {
+        console.error('Error creating note:', error);
+        alert('Error creating note. Please try again.');
+      });
+    }
+  }
+};
+</script>
+
+  
+<style>
+  /* Add styling for the create note form */
+  
+</style>
+  
