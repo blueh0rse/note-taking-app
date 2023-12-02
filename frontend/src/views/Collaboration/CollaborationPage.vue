af6154b38303f422f7b51c841dd01066f2527b79
Jpmorcos <jpmorcosdouaihy2002@hotmail.com>
2023-12-02 18:16:06 -0500

    Add shared notes display to CollaborationPage

--- frontend/src/views/Collaboration/CollaborationPage.vue
+++ frontend/src/views/Collaboration/CollaborationPage.vue
@@ -1,17 +1,25 @@
 <template>
     <div class="collaboration-page">
-      <!-- Collaboration content goes here -->
+        <h2>Shared Notes</h2>
+        <ul>
+            <li v-for="note in sharedNotes" :key="note.id">{{ note.title }}</li>
+        </ul>
+        <!-- Additional collaboration features here -->
     </div>
-  </template>
+</template>
   
-  <script>
-  export default {
-    name: 'CollaborationPage',
-    // Component logic goes here
-  }
-  </script>
+<script>
+export default {
+    data() {
+        return {
+            sharedNotes: [] // This will be fetched from backend
+        };
+    },
+    // Additional methods for collaboration features
+}
+</script>
   
-  <style scoped src="./CollaborationStyles.css">
-  /* CSS specific to CollaborationPage */
-  </style>
+<style scoped src="./CollaborationStyles.css">
+/* CSS specific to CollaborationPage */
+</style>
   
