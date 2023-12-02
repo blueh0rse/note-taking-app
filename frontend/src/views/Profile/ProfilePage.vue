9087bccd54c9a56e1cf30c3c2ca2dc02ba87533f
Jpmorcos <jpmorcosdouaihy2002@hotmail.com>
2023-12-02 17:56:18 -0500

    Initialize Profile and Settings page structure

---
+++ frontend/src/views/Profile/ProfilePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="profile-page">
+        <!-- Profile and settings content goes here -->
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'ProfilePage',
+    // Component logic goes here
+}
+</script>
+  
+<style scoped src="./ProfileStyles.css">
+/* CSS specific to ProfilePage */
+</style>
+  
