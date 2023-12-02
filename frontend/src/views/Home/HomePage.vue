52d2c4594819b34de07b8dc3c176bfc6f925c395
Jpmorcos <jpmorcosdouaihy2002@hotmail.com>
2023-12-02 14:12:27 -0500

    Set up homepage structure

---
+++ frontend/src/views/Home/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home-page">
+      <!-- Homepage content goes here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage',
+    // Component logic goes here
+  }
+  </script>
+  
+  <style scoped src="./HomeStyles.css">
+  /* CSS specific to HomePage */
+  </style>
+  
