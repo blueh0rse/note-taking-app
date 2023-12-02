7d265ada3fc3b8ca31a423c84c7e165b459b006f
Jpmorcos <jpmorcosdouaihy2002@hotmail.com>
2023-12-02 18:14:33 -0500

    Initialize Collaboration page structure

---
+++ frontend/src/views/Collaboration/CollaborationPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="collaboration-page">
+      <!-- Collaboration content goes here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CollaborationPage',
+    // Component logic goes here
+  }
+  </script>
+  
+  <style scoped src="./CollaborationStyles.css">
+  /* CSS specific to CollaborationPage */
+  </style>
+  
