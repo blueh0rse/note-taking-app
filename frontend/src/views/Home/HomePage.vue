67f67be1057c171c64d33975dd63287c27bf6537
Jpmorcos <jpmorcosdouaihy2002@hotmail.com>
2023-12-02 18:38:02 -0500

    Add authentication form to HomePage

--- frontend/src/views/Home/HomePage.vue
+++ frontend/src/views/Home/HomePage.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="home-page">
-      <Navbar />
-      <LoginSignup />
-      <!-- Other homepage content -->
-    </div>
-  </template>
+  <div>
+    <!-- Toggle between Login and Signup -->
+    <form @submit.prevent="handleAuth">
+      <!-- Form fields for username, password -->
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
   
 <script>
 import Navbar from '@/components/Navbar.vue'
@@ -13,7 +15,19 @@ export default {
     name: 'HomePage',
     components: {
         Navbar
+    },
+    data() {
+    return {
+      username: '',
+      password: '',
+      // other relevant data
+    };
+  },
+  methods: {
+    handleAuth() {
+      // Authentication logic
     }
+  }
 }
 </script>
   
