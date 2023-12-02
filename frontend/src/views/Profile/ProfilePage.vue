596fc0c8060fc259805eafff44e5affa5c25258d
Jpmorcos <jpmorcosdouaihy2002@hotmail.com>
2023-12-02 17:58:50 -0500

    Add user profile features to ProfilePage

--- frontend/src/views/Profile/ProfilePage.vue
+++ frontend/src/views/Profile/ProfilePage.vue
@@ -1,13 +1,31 @@
 <template>
     <div class="profile-page">
-        <!-- Profile and settings content goes here -->
+      <h2>User Profile</h2>
+      <form @submit.prevent="updateProfile">
+        <!-- Form fields for profile details like name, email -->
+        <button type="submit">Update Profile</button>
+      </form>
+      <!-- Additional settings if needed -->
     </div>
-</template>
+  </template>
+  
   
 <script>
 export default {
-    name: 'ProfilePage',
-    // Component logic goes here
+  data() {
+    return {
+      userProfile: {
+        name: '',
+        email: '',
+        // other profile fields
+      }
+    };
+  },
+  methods: {
+    updateProfile() {
+      // Logic to update profile details
+    }
+  }
 }
 </script>
   
