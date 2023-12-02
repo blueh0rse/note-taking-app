52fb2888f7e69b11cc47846bbe26c0d5e91e5447
Jpmorcos <jpmorcosdouaihy2002@hotmail.com>
2023-12-02 15:26:56 -0500

    Add dashboard layout and note card component

---
+++ frontend/src/views/Dashboard/DashboardPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="dashboard">
+        <button @click="createNote">Create Note</button>
+        <div class="notes-grid">
+            <NoteCard v-for="note in notes" :key="note.id" :note="note" />
+        </div>
+    </div>
+</template>
+  
+<script>
+import NoteCard from '@/components/NoteCard.vue';
+
+export default {
+    name: 'DashboardPage',
+    components: {
+        NoteCard
+    },
+    data() {
+        return {
+            notes: [] // This will be fetched from backend
+        };
+    },
+    methods: {
+        createNote() {
+            // Logic to create a new note
+        }
+    }
+}
+</script>
+  
