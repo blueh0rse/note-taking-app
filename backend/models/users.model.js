6935e11caf51e40ba0fadc32f399d471c5be45f8
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2023-12-03 00:59:54 +0100

    change username importance

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
   password: { type: String, required: true },
 });
 
