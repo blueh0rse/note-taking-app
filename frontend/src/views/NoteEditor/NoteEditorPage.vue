01b5d0ff7954e8d28e5f3675e30d6f5b5bea4ef4
Jpmorcos <jpmorcosdouaihy2002@hotmail.com>
2023-12-02 15:47:38 -0500

    Initialize Note Editor page structure

---
+++ frontend/src/views/NoteEditor/NoteEditorPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="note-editor">
+        <!-- Note editor content goes here -->
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'NoteEditorPage',
+    // Component logic goes here
+}
+</script>
+  
+<style scoped src="./NoteEditorStyles.css">
+/* CSS specific to NoteEditorPage */
+</style>
+  
