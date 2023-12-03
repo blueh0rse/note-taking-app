fc58e711c2a6a7c5675f67dfaac39fc09e9f9fef
Jpmorcos <jpmorcosdouaihy2002@hotmail.com>
2023-12-03 15:53:40 -0500

    Test version HomePage

--- frontend/src/views/Home/HomePage.vue
+++ frontend/src/views/Home/HomePage.vue
@@ -1,28 +1,31 @@
 <template>
   <!-- Navbar is hidden when not logged in -->
-  <div class="login-section">
-    <h2>Login</h2>
-    <form @submit.prevent="login">
-      <input type="email" v-model="email" placeholder="Email" required />
+  <div class="auth-forms">
+    <h2 v-if="!isSignupVisible">Login</h2>
+    <h2 v-if="isSignupVisible">Sign Up</h2>
+    <form @submit.prevent="login" v-if="!isSignupVisible">
+      <input type="text" v-model="loginIdentifier" placeholder="Email or Username" required />
       <input type="password" v-model="password" placeholder="Password" required />
+      <p v-if="loginError" class="error">{{ loginError }}</p>
       <button type="submit">Login</button>
     </form>
-    <button @click="showSignupForm">Sign Up</button>
+    <button @click="showSignupForm(); resetLoginForm()" v-if="!isSignupVisible">Sign Up</button>
   </div>
-  
-  <div v-if="isSignupVisible" class="signup-section">
-    <h2>Sign Up</h2>
+  <div v-if="isSignupVisible" class="auth-forms">
     <form @submit.prevent="signup">
-      <input type="email" v-model="signupEmail" placeholder="Email" required 
-             pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" />
-      <input type="password" v-model="signupPassword" placeholder="Password" 
-             required pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{10,}$" />
+      <input type="text" v-model="signupUsername" placeholder="Username" required />
+      <input type="email" v-model="signupEmail" placeholder="Email" required
+        pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" />
+      <input type="password" v-model="signupPassword" placeholder="Password" required
+        pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{10,}$" />
       <input type="password" v-model="signupPasswordConfirm" placeholder="Confirm Password" required />
+      <p class="password-instruction">Please choose a password that contains 10 characters, including a number and a
+        special character.</p>
+      <p v-if="signupError" class="error">{{ signupError }}</p>
       <button type="submit">Sign Up</button>
+      <button @click="resetSignupForm">Cancel</button>
     </form>
   </div>
-  <p v-if="loginError" class="error">{{ loginError }}</p>
-  <p v-if="signupError" class="error">{{ signupError }}</p>
 </template>
 
 <script>
@@ -31,23 +34,22 @@ import authService from '@/services/authService';
 export default {
   data() {
     return {
-      email: '',
+      loginIdentifier: '',
       password: '',
       isSignupVisible: false,
+      signupUsername: '',
       signupEmail: '',
       signupPassword: '',
       signupPasswordConfirm: '',
       loginError: '',
       signupError: '',
-      // Signup form data
     };
   },
   methods: {
     async login() {
       try {
-        const response = await authService.login(this.loginUsername, this.loginPassword);
+        const response = await authService.login(this.loginIdentifier, this.loginPassword);
         this.loginError = ''; // Clear any previous error
-        // Redirect to dashboard or update Vuex store
         this.$store.commit('setUser', response.data.user); // Update Vuex store
         this.$router.push('/dashboard'); // Redirect to dashboard
       } catch (error) {
@@ -71,6 +73,19 @@ export default {
     },
     showSignupForm() {
       this.isSignupVisible = true;
+    },
+    resetSignupForm() {
+      this.signupUsername = '';
+      this.signupEmail = '';
+      this.signupPassword = '';
+      this.signupPasswordConfirm = '';
+      this.signupError = '';
+      this.isSignupVisible = false;
+    },
+    resetLoginForm() {
+      this.loginIdentifier = '';
+      this.loginPassword = '';
+      this.loginError = '';
     },
   }
 }
