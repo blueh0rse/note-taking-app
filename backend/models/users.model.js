9b72cfe9cfbf4bfd26eb00fa1d2308ce4c921416
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2023-12-04 01:51:12 +0100

    Update models

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true },
   password: { type: String, required: true },
+  role: { type: String, required: true, default: "user" },
 });
 
 module.exports = mongoose.model("User", userSchema);
